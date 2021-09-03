885dadf3084d22d3edcfc7ae9ae487f2249199ca
Oscar Guerrero G <oguerrerog@gmail.com>
2021-09-02 23:17:03 -0400

    UPDATE

--- app/models.py
+++ app/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # agregue claves y valores al diccionario de errores para cada campo no válidocopy
+        if len(postData['title']) < 5:
+            errors["title"] = "DATO OBLIGATORIO: Titulo debe tener al menos 5 caracteres"
+        if len(postData['network']) < 3:
+            errors["network"] = "DATO OBLIGATORIO: ID del canal debe tener al menos 2 caracteres"
+        if len(postData['date']) < 1:
+            errors["date"] = "DATO OBLIGATORIO: Indicar la fecha de la creacion del canal"
+        if len(postData['description']) > 0 & len(postData['description']) < 10:
+            errors["description"] = "ATENCION: Ingrese una descripcion de al menos 10 caracteres o dejar vacio"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -7,6 +21,7 @@ class Show(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
     def __repr__(self):
         return f"{self.title} - {self.network} - {self.date}"
--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from app.models import Show
 from django.contrib import messages
-from django.utils import timezone
-from datetime import datetime
+
 
 # Create your views here.
 def index(request):
@@ -20,25 +19,41 @@ def newShow(request):
 
 
 def createShow(request):
-    print(request.POST['date'])
-    
-    # Agregamos formulario a la BD
-    Show.objects.create(
-        title = request.POST['title'],
-        network=request.POST['network'],
-        date=request.POST['date'], 
-        description=request.POST['description'])
-    
-    #Obtenemos ID 
-    id = Show.objects.last().id
-    showTitle = Show.objects.get(id=id).title
+    print(request.POST['title'])
 
-    messagePopup = f'El TV Show "{showTitle}", ha sido creado correctamente'
-    print(messagePopup)
-    messages.success(request, messagePopup)
+    errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en élcopy
+    if len(errors) > 0:
+        # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+        for keyNoShow, value in errors.items():
+            messages.error(request, value)
+        # redirigir al usuario al formulario para corregir los errores
+        context = {
+            "data_show_title": request.POST['title'],
+            "data_show_network": request.POST['network'],
+            "data_show_date": request.POST['date'],
+            "data_show_description": request.POST['description']
+        }
+        return render(request, "new_show.html", context)
+    else:
+    
+        # Agregamos formulario a la BD
+        Show.objects.create(
+            title = request.POST['title'],
+            network=request.POST['network'],
+            date=request.POST['date'], 
+            description=request.POST['description'])
+        
+        #Obtenemos ID 
+        id = Show.objects.last().id
+        showTitle = Show.objects.get(id=id).title
 
-    # Redireccion a pagina mostrando los datos ingresados
-    return redirect(f'/shows/{id}')
+        messagePopup = f'El TV Show "{showTitle}", ha sido creado correctamente'
+        print(messagePopup)
+        messages.success(request, messagePopup)
+
+        # Redireccion a pagina mostrando los datos ingresados
+        return redirect(f'/shows/{id}')
 
 
 
@@ -47,9 +62,9 @@ def deleteShow(request, id):
     showTitle = Show.objects.get(id=id).title
     elimina = Show.objects.get(id=id)
     elimina.delete()
-    messagePopup = f'Exito al Eliminar el TV Show "{showTitle}"'
+    messagePopup = f'Exito al Eliminar: TV Show "{showTitle}"'
     print(messagePopup)
-    messages.warning(request, messagePopup)
+    messages.error(request, messagePopup)
 
     return redirect(f'/shows')
 
@@ -63,24 +78,34 @@ def editShow(request, id):
 
 
 def saveShow(request, id):
+
+    errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en élcopy
+    if len(errors) > 0:
+        # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+        for keyNoShow, value in errors.items():
+            messages.error(request, value)
+        # redirigir al usuario al formulario para corregir los errores
+        return redirect(f'/shows/{id}/edit')
+    else:
     
-    fecha = request.POST['date']
-    title = request.POST['title']
-    network = request.POST['network']
-    description = request.POST['description']
-    print(f"\n\nTitulo: {title}\nCanal: {network}\nFecha: {fecha}\nDescripcion: {description}\n\n")
-    actualiza = Show.objects.get(id=id)
-    actualiza.title = title
-    actualiza.network = network
-    actualiza.date = fecha
-    actualiza.description = description
-    actualiza.save()
-    
-    # Muestra mensaje Exito
-    messagePopup = f'Exito al Modificar el TV Show "{title}"'
-    print(messagePopup); messages.success(request, messagePopup)
-    
-    return redirect(f'/shows{id}')
+        fecha = request.POST['date']
+        title = request.POST['title']
+        network = request.POST['network']
+        description = request.POST['description']
+        print(f"\n\nTitulo: {title}\nCanal: {network}\nFecha: {fecha}\nDescripcion: {description}\n\n")
+        actualiza = Show.objects.get(id=id)
+        actualiza.title = title
+        actualiza.network = network
+        actualiza.date = fecha
+        actualiza.description = description
+        actualiza.save()
+        
+        # Muestra mensaje Exito
+        messagePopup = f'Exito al Modificar el TV Show:</br> "{title}"'
+        print(messagePopup); messages.success(request, messagePopup)
+        
+        return redirect(f'/shows/{id}')
 
 
 
